d99c08f636d3eedfb601419127d8b650ce7ffeea
gwem <cofinalsubnets@gmail.com>
2024-10-23 23:49:28 -0400

    leave alone

--- gwen.js
+++ gwen.js
@@ -21,47 +21,45 @@ const gwen_eval = (() => {
       ;
 
   class Symbol { constructor(nom) { this.nom = nom; } }
-  const
-    { isArray } = Array,
-    symbols = {},
-    intern = str => (sym => sym ? sym : (symbols[str] = new Symbol(str)))(symbols[str]),
-    [Quote, Lambda, Define, Cond, Begin] = ['`', '\\', ':', '?', ','].map(intern),
-    ev = x => l => {
-      if (x instanceof Symbol) return l(x);
-      if (!isArray(x)) return x;
-      if (x.length == 0) return 0;
-      const [x0, ...a] = x;
-      // examine the first item to see if it's a special form
-      if (x0 == Quote) return a[0];
-      if (x0 == Begin) return a.reduce((_, x) => ev(x)(l), 0);
-      if (x0 == Lambda) return a.slice(0, -1).reduceRight(
-        (f, arg) => l => x => f(y => y === arg ? x : l(y)),
-        ev(a[a.length-1])
-      )(l);
+  const { isArray } = Array,
+        intern = (sym => s => (y => y ? y : (sym[s] = new Symbol(s)))(sym[s]))({}),
+        [Quote, Lambda, Define, Cond, Begin] = ['`', '\\', ':', '?', ','].map(intern);
+  const ev = x => l => {
+    if (x instanceof Symbol) return l(x);
+    if (!isArray(x)) return x;
+    if (x.length == 0) return 0;
+    const [x0, ...a] = x;
+    // examine the first item to see if it's a special form
+    if (x0 == Quote) return a[0];
+    if (x0 == Begin) return a.reduce((_, x) => ev(x)(l), 0);
+    if (x0 == Lambda) return a.slice(0, -1).reduceRight(
+      (f, arg) => l => x => f(y => y === arg ? x : l(y)),
+      ev(a[a.length-1])
+    )(l);
 
-      if (x0 == Cond) for (let i = 0;; i += 2) {
-        if (i == a.length) return 0; // no default case => 0
-        if (i == a.length - 1) return ev(a[i])(l); // default case
-        if (ev(a[i])(l)) return ev(a[i + 1])(l); // conditional branch
-      }
+    if (x0 == Cond) for (let i = 0;; i += 2) {
+      if (i == a.length) return 0; // no default case => 0
+      if (i == a.length - 1) return ev(a[i])(l); // default case
+      if (ev(a[i])(l)) return ev(a[i + 1])(l); // conditional branch
+    }
 
-      if (x0 == Define) for (let i = 0, bs = [];; i += 2) {
-        if (i == a.length) a.push(0); // no inner expression => 0
-        // last expression, call bindings and eval
-        if (i == a.length - 1) return bs.forEach(b=>b(l)), ev(a[i])(l);
-        // key, value, closure binding, current store
-        let k = a[i], v = a[i+1], cb, l0 = l;
-        // desugar (: (f a b) (g a b)) to (: f (\ a b (g a b)))
-        while (isArray(k)) v = [Lambda, ...k.slice(1), v], k = k[0];
-        l = x => x === k ? cb : l0(x); // update store
-        bs.push(l => cb = ev(v)(l)); // push bind function
-      }
+    if (x0 == Define) for (let i = 0, bs = [];; i += 2) {
+      if (i == a.length) a.push(0); // no inner expression => 0
+      // last expression, call bindings and eval
+      if (i == a.length - 1) return bs.forEach(b=>b(l)), ev(a[i])(l);
+      // key, value, closure binding, current store
+      let k = a[i], v = a[i+1], cb, l0 = l;
+      // desugar (: (f a b) (g a b)) to (: f (\ a b (g a b)))
+      while (isArray(k)) v = [Lambda, ...k.slice(1), v], k = k[0];
+      l = x => x === k ? cb : l0(x); // update store
+      bs.push(l => cb = ev(v)(l)); // push bind function
+    }
 
-      // map eval fold apply
-      return x.map(x => ev(x)(l)).reduce(
-        (f, x) => typeof(f) === 'function' ? f(x) : f
-      );
-    };
+    // map eval fold apply
+    return x.map(x => ev(x)(l)).reduce(
+      (f, x) => typeof(f) === 'function' ? f(x) : f
+    );
+  };
 
   const
     G = (() => {
