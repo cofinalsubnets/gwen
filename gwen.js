f5dabdeb9f451bab4ea94275b2244d5f87ba7560
gwem <cofinalsubnets@gmail.com>
2024-10-27 16:32:08 -0400

    rm old ev fn

--- gwen.js
+++ gwen.js
@@ -26,7 +26,7 @@ const gwen = (() => {
 
       if (x0 === Let) for (let i = 0, b = l => l, m = b;; i += 2) {
         if (i === a.length) a.push(0); // no inner expression => eval 0
-        if (i === a.length - 1) return x = ana(a[i]), l => x(b(m(l))); // call b with m and eval last expression
+        if (i === a.length - 1) return x = ana(a[i]), l => x(b(m(l))); // compose and return
         let k = a[i], v = a[i+1], cb, m0 = m, b0 = b; // key, value, closure binding, current store
         while (isArray(k)) v = [Lambda, ...k.slice(1), v], k = k[0]; // desugar (: (f a b) (g a b)) to (: f (\ a b (g a b)))
         v = ana(v);
@@ -36,61 +36,6 @@ const gwen = (() => {
 
       const ap = (f, x) => typeof(f) === 'function' ? f(x) : f;
       return x.map(ana).reduce((f, x) => l => ap(f(l), x(l)), l => x => x);
-    },
-
-    ev = x => l => {
-      if (typeof(x) === 'symbol') return l(x); // symbols get looked up
-      if (!isArray(x)) return x; // other atoms get returned
-      if (!x.length) return 0; // empty list same as zero
-      // it's a list so examine the first item to see if it's a special form
-      const [x0, ...a] = x;
-      // first couple forms are easy...
-      // quote returns the thing
-      if (x0 === Quote) return a[0];
-      // begin evals in order and return the last one
-      if (x0 === Begin) return a.reduce((_, x) => ev(x)(l), 0);
-
-      // lambda expression build curried function that
-      // bind argument to symbol in current environment
-      // then eval the inner expression
-      if (x0 === Lambda) return a.slice(0, -1).reduceRight(
-        (f, arg) => l => x => f(y => y === arg ? x : l(y)),
-        ev(a[a.length-1])
-      )(l);
-
-      // conditionals try to find a branch that passes, very simple
-      if (x0 === Cond) for (let i = 0;; i += 2) {
-        if (i === a.length) return 0; // no default case => 0
-        if (i === a.length - 1) return ev(a[i])(l); // default case
-        if (ev(a[i])(l)) return ev(a[i + 1])(l); // conditional branch
-      }
-
-      // this is the hard one ... let expressions ...
-      // we want to bind everything first and then evaluate the
-      // definitions in order, which gives the right behavior
-      // for both sequential binding and mutual recursion.
-      //
-      // there are two accumulator variables in the loop, b and m.
-      // at the end of the loop b is called with m.
-      // - b is identity function of environment with side effect of
-      //   evaluating definitions and placing the value in a local
-      //   enclosed  variable (cb)
-      // - m is an updated environment referring to cb.
-      //
-      // hopefully every cb will have been initialized by the time the
-      // variable gets looked up. if not then the value will be undefined,
-      // same as javascript.
-      if (x0 === Let) for (let i = 0, b = l => l, m = l;; i += 2) {
-        if (i === a.length) return ev(0)(b(m)); // no inner expression => eval 0
-        if (i === a.length - 1) return ev(a[i])(b(m)); // call b with m and eval last expression
-        let k = a[i], v = a[i+1], cb, m0 = m, b0 = b; // key, value, closure binding, current store
-        while (isArray(k)) v = [Lambda, ...k.slice(1), v], k = k[0]; // desugar (: (f a b) (g a b)) to (: f (\ a b (g a b)))
-        m = x => x === k ? cb : m0(x); // new store defaults to old store
-        b = l => (cb = ev(v)(b0(l)), l); // new binding function calls old binding function
-      }
-
-      // not a special form, regular function expression. map eval fold apply
-      return x.map(x => ev(x)(l)).reduce((f, x) => typeof(f) === 'function' ? f(x) : f);
     },
 
     // expression parser
