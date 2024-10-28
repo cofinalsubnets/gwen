c9ff1cb7dba31038aaa09089a7f9ad4582449b1d
gwem <cofinalsubnets@gmail.com>
2024-10-28 08:56:38 -0400

    restore non-analyzing evaluator for debugging

--- gwen.js
+++ gwen.js
@@ -5,7 +5,7 @@ const gwen = (() => {
     [Quote, Lambda, Let, Cond, Begin] = ['`', '\\', ':', '?', ','].map(Symbol.for),
 
     // evaluator: expression -> environment -> expression
-    ev = x => {
+    ana = x => {
       if (typeof(x) === 'symbol') return l => l(x); // symbols get looked up
       if (!isArray(x)) return l => x; // non-symbol non-lists get returned
       if (!x.length) return l => 0; // empty list same as zero
@@ -13,7 +13,7 @@ const gwen = (() => {
       // quote returns the thing
       if (x[0] === Quote) return l => x[1];
       // sequence evaluates in order and returns the last value
-      if (x[0] === Begin) return x.slice(1).map(ev).reduce(
+      if (x[0] === Begin) return x.slice(1).map(ana).reduce(
         (a, b) => l => (a(l), b(l)),
         l => 0
       );
@@ -21,15 +21,15 @@ const gwen = (() => {
       // that returns the described function
       if (x[0] === Lambda) return x.slice(1, -1).reduceRight(
         (f, arg) => l => x => f(y => y === arg ? x : l(y)),
-        ev(x[x.length-1])
+        ana(x[x.length-1])
       );
 
       // cond folds the branches into a function of the default expression that returns
       // a function of environment that evaluates to one branch as you would expect
       if (x[0] === Cond) for (let i = 1, f = x => x;; i += 2) {
         if (i === x.length) x.push(0); // no default case => 0
-        if (i === x.length - 1) return f(ev(x[i])); // default case
-        const ant = ev(x[i]), con = ev(x[i+1]), f0 = f;
+        if (i === x.length - 1) return f(ana(x[i])); // default case
+        const ant = ana(x[i]), con = ana(x[i+1]), f0 = f;
         f = x => f0(l => ant(l) ? con(l) : x(l));
       }
 
@@ -42,11 +42,11 @@ const gwen = (() => {
       // expression.
       if (x[0] === Let) for (let i = 1, b = l => l, m = b;; i += 2) {
         if (i === x.length) x.push(0); // no inner expression => eval 0
-        if (i === x.length - 1) return x = ev(x[i]), l => x(b(m(l))); // compose and return
+        if (i === x.length - 1) return x = ana(x[i]), l => x(b(m(l))); // compose and return
         const m0 = m, b0 = b; // 
         let k = x[i], v = x[i+1], cb; // key, value, closure binding
         while (isArray(k)) v = [Lambda, ...k.slice(1), v], k = k[0]; // desugar (: (f a b) (g a b)) to (: f (\ a b (g a b)))
-        v = ev(v);
+        v = ana(v);
         m = l => x => x === k ? cb : m0(l)(x); // new store defaults to old store
         b = l => (cb = v(b0(l)), l); // new binding function calls old binding function
       }
@@ -54,7 +54,7 @@ const gwen = (() => {
       // it is not a special form, reduce to a function that
       // evals and applies in the given environment
       const ap = (f, x) => typeof(f) === 'function' ? f(x) : f;
-      return x.map(ev).reduce((f, x) => l => ap(f(l), x(l)), l => x => x);
+      return x.map(ana).reduce((f, x) => l => ap(f(l), x(l)), l => x => x);
     },
 
     // expression parser
@@ -109,6 +109,43 @@ const gwen = (() => {
         ["B", a=>!isArray(a)||a.length<2?0:a.slice(1)],
       ].reduce((g, [k, v]) => ((g[Symbol.for(k)] = v), g), {})),
 
+    ev = x => l => {
+      // look up a symbol
+      if (typeof(x) === 'symbol') return l(x);
+      // return self quoting data
+      if (!isArray(x)) return x;
+      // empty list same as zero
+      if (x.length == 0) return 0;
+      // examine the first item to see if it's a special form
+      const [x0, ...a] = x;
+      if (x0 === Quote) return a[0];
+      if (x0 === Begin) return a.reduce((_, x) => ev(x)(l), 0);
+
+      if (x0 === Lambda) return a.slice(0, -1).reduceRight(
+        (f, arg) => l => x => f(y => y === arg ? x : l(y)),
+        ev(a[a.length-1])
+      )(l);
+
+      if (x0 === Cond) for (let i = 0;; i += 2) {
+        if (i == a.length) return 0; // no default case => 0
+        if (i == a.length - 1) return ev(a[i])(l); // default case
+        if (ev(a[i])(l)) return ev(a[i + 1])(l); // conditional branch
+      }
+
+      if (x0 === Let) for (let i = 0, b = l => l, m = l;; i += 2) {
+        if (i == a.length) a.push(0); // no inner expression => eval 0
+        if (i == a.length - 1) return ev(a[i])(b(m)); // call b with m and eval last expression
+        let k = a[i], v = a[i+1], cb, m0 = m, b0 = b; // key, value, closure binding, current store
+        while (isArray(k)) v = [Lambda, ...k.slice(1), v], k = k[0]; // desugar (: (f a b) (g a b)) to (: f (\ a b (g a b)))
+        m = x => x === k ? cb : m0(x); // new store defaults to old store
+        b = l => (cb = ev(v)(b0(l)), l); // new binding function calls old binding function
+      }
+
+      return x.map(x => ev(x)(l)).reduce(
+        (f, x) => typeof(f) === 'function' ? f(x) : f
+      );
+    },
+
     // print function
     gwen_show = x => {
       switch (typeof(x)) {
@@ -119,9 +156,10 @@ const gwen = (() => {
     };
 
   return {
-    eval: x => ev(x)(global_env),
+    eval: x => ana(x)(global_env),
     read: gwen_read,
     show: gwen_show,
+    ev: x => ev(x)(global_env),
   }
 })();
 
