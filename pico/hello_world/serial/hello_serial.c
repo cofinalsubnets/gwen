537882c9e33a7eb7f467822068dea3ee7708762a
Gwen W <gwendolyn.whelan@gmail.com>
2025-10-02 16:07:40 -0400

    bwu

--- pico/hello_world/serial/hello_serial.c
+++ pico/hello_world/serial/hello_serial.c
@@ -16,15 +16,12 @@ uintptr_t g_clock(void) {
   return get_absolute_time(); }
 
 #define g_static_size  (1<<14)
-static void *g_static_kmalloc(g_core *f, size_t n) {
-  static g_word g_static_pool[2][g_static_size];
-  return f ? NULL : g_static_pool; }
-static void g_static_kfree(g_core *f, void *_) {}
+static g_word g_static_pool[2][g_static_size];
 
 int main() {
     stdio_init_all();
     printf("Hello, world!\n");
-    g_core *f = g_ini_m_n(g_static_kmalloc, g_static_kfree, g_static_size);
+    g_core *f = g_ini_static(g_static_size, g_static_pool);
     printf("got f@0x%x", (unsigned int) f);
     if (g_ok(f)) f = g_evals_(f, boot),
                  printf("post boot got f@0x%x\n", (unsigned int) f);
